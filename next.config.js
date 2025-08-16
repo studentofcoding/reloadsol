06d283995de03295537091444c35f9f7d30f0a34
Yonathan Evan Christy <yonathanevan@gmail.com>
2025-08-16 16:22:57 +0800

    Add missing / for privy

--- next.config.js
+++ next.config.js
@@ -92,7 +92,7 @@ const nextConfig = {
               font-src 'self' fonts.gstatic.com;
               img-src 'self' data: blob: https:;
               connect-src 'self' *.supabase.co *.supabase.in *.shyft.to *.helius-rpc.com *.solana.com *.jup.ag *.jupiter-swap.com https://auth.privy.io/ wss://relay.walletconnect.com wss://relay.walletconnect.org wss://www.walletlink.org https://*.rpc.privy.systems https://explorer-api.walletconnect.com wss: https:;
-              frame-src 'self' https://auth.privy.io https://www.gmgn.cc https://gmgn.cc https://terminal.jup.ag/ https://verify.walletconnect.com https://verify.walletconnect.org https://challenges.cloudflare.com;
+              frame-src 'self' https://auth.privy.io/ https://www.gmgn.cc https://gmgn.cc https://terminal.jup.ag/ https://verify.walletconnect.com https://verify.walletconnect.org https://challenges.cloudflare.com;
               object-src 'none';
               base-uri 'self';
               form-action 'self';
